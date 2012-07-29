a64d23a78cc8d549b1b710ea7e3ef77c0e1b78b5
StarDriver <github@dominikpohl.de>
2012-07-29 16:07:18 +0200

    r4 - v0.04
    Created file for Javascript Code (code.js)
    index.php links to code.js

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>FCP [@<?php echo $_SERVER["SERVER_NAME"]; ?>]</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<script type="text/javascript" src="./js/jquery.js"></script>
+	<script type="text/javascript" src="./js/code.js"></script>
 </head>
 <body>
 
