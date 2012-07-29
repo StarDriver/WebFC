1323e072159e2595f012fa532761cdc4519c19f4
StarDriver <github@dominikpohl.de>
2012-07-29 15:38:35 +0200

    r2 - v0.02
    Created a index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FCP [@<?php echo $_SERVER["SERVER_NAME"]; ?>]</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+
+
+
+</body>
+</html>
