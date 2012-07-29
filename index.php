7425b6474abab20bd470dcaf2d87c72516849892
StarDriver <github@dominikpohl.de>
2012-07-29 16:02:06 +0200

    r3 - v0.03
    Added jQuery JS Library [jquery.com]
    index.php now includes jquery.js

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>FCP [@<?php echo $_SERVER["SERVER_NAME"]; ?>]</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<script type="text/javascript" src="./js/jquery.js"></script>
 </head>
 <body>
 
